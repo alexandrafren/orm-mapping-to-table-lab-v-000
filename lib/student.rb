7686abeca1ff4fb5bd7251f7dcedde24e49749ee
Alexandra Fren <alexandra.fren@gmail.com>
2018-07-19 19:30:39 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -12,12 +12,11 @@ class Student
   end
 
   def self.create_table
-    sql = <-SQL
+    sql = <<-SQL
     CREATE TABLE IF NOT EXISTS students (
       id INTEGER PRIMARY KEY,
       name TEXT,
-      album TEXT
-    )
+      album TEXT)
     SQL
     DB[:conn].execute(sql)
 end
